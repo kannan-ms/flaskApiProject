1b4ebb75c150d3abd33e947cf535aacb73df2680
KANNAN M <140005183+kannan-ms@users.noreply.github.com>
2026-04-28 07:14:34 +0530

    commands changed

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify, request
 
 app = Flask(__name__)
 
-# In-memory database (list of dictionaries)
+# In-memory database (list of dictionaries) are available in the books array of dict
 books = [
     {"id": 1, "title": "The Great Gatsby", "author": "F. Scott Fitzgerald"},
     {"id": 2, "title": "1984", "author": "George Orwell"},
